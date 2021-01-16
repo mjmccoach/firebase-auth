052b90d3fdbeb01c014d4da7908f25d55ff9dc62
mjmccoach <mmccoach88@gmail.com>
2021-01-16 15:02:39 +0000

    Implemented react router and have working routes for Dashboard, Login and Signup. Dashboard and Login are currently empty

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import React from "react"
 import Signup from "./components/Signup"
+import Dashboard from "./components/Dashboard"
+import Login from "./components/Login"
 import { Container } from "react-bootstrap";
 import { AuthProvider } from './context/AuthContext'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   
   return (
-    <AuthProvider>
       <Container className="d-flex align-items-center justinfy content center"
       style={{ minHeight: "100vh"}}>
       
         <div className="w-100" style ={{maxwidth: "400px"}}>
-        <Signup />
+          <Router>
+            <AuthProvider>
+              <Switch>
+                <Route exact path="/" component={Dashboard} />
+                <Route path="/signup" component={Signup} />
+                <Route path="/login" component={Login} />
+              </Switch>
+            </AuthProvider>
+          </Router>
+  
         </div>
       
       </Container>
-    </AuthProvider>
+    
   );
 }
 
